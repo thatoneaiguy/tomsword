9ba49ab187f6ef4d66a1bac991cb329f1331c788
NotMarshy1 <Animations.by.alby@gmail.com>
2023-07-30 21:22:06 +0100

    gui textures

--- src/main/java/net/thatoneaiguy/tomsword/item/ModItems.java
+++ src/main/java/net/thatoneaiguy/tomsword/item/ModItems.java
@@ -31,7 +31,7 @@ public class ModItems {
             new Item(new FabricItemSettings().group(ModItemGroup.TOMSWORD)));
     public static final Item STARWEAVER = registerItem("starweaver",
             new SwordItem(ToolMaterials.NETHERITE, 3, -2.4F, new FabricItemSettings().group(ModItemGroup.TOMSWORD)));
-    public static final Item SOULE_SCYTHE = registerItem("soul_scythe",
+    public static final Item SOULE_SCYTHE = registerItem("soule_scythe",
             new SwordItem(ToolMaterials.NETHERITE, 3, -2.1F, new FabricItemSettings().group(ModItemGroup.TOMSWORD)));
 
 
--- src/main/java/net/thatoneaiguy/tomsword/mixin/ItemRendererMixin.java
+++ src/main/java/net/thatoneaiguy/tomsword/mixin/ItemRendererMixin.java
@@ -1,5 +1,6 @@
-/*package net.thatoneaiguy.tomsword.mixin;
+package net.thatoneaiguy.tomsword.mixin;
 
+import net.minecraft.client.render.model.json.ModelTransformation.Mode;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.ItemModels;
 import net.minecraft.client.render.item.ItemRenderer;
@@ -16,18 +17,20 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ItemRenderer.class)
 public class ItemRendererMixin {
-    private static final ModelIdentifier INVENTORY_MODEL = new ModelIdentifier("tomsword", "item/smoldia", "inventory");
+    private static final ModelIdentifier DIA_INVENTORY_MODEL = new ModelIdentifier("tomsword:smoldia#inventory");
     @Shadow
     private @Final ItemModels models;
 
     @ModifyVariable(method = "renderItem", at = @At("HEAD"), argsOnly = true)
-    private BakedModel modid_guiModel(BakedModel model, ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel modelAgain) {
-        boolean bl = renderMode == ModelTransformationMode.GUI || renderMode == ModelTransformationMode.GROUND || renderMode == ModelTransformationMode.FIXED;
+    private BakedModel tomsword_guiModel(BakedModel model, ItemStack stack, Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel modelAgain) {
+        boolean bl = renderMode == Mode.GUI || renderMode == Mode.GROUND || renderMode == Mode.FIXED;
         if (bl) {
             if (stack.isOf(ModItems.DIAMOND_LONGSWORD)) {
-                return models.getModelManager().getModel(INVENTORY_MODEL);
+                return models.getModelManager().getModel(DIA_INVENTORY_MODEL);
             }
         }
+
         return model;
     }
-}*/
+}
+
