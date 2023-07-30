a9b4155bb91c5e719e4bf0c58bae9f875c1b6f26
NotMarshy1 <71119717+NotMarshy1@users.noreply.github.com>
2023-07-30 23:06:49 +0100

    Update TomswordClient.java
    GUI TEXTURES

--- src/main/java/net/thatoneaiguy/tomsword/TomswordClient.java
+++ src/main/java/net/thatoneaiguy/tomsword/TomswordClient.java
@@ -7,6 +7,7 @@ import net.thatoneaiguy.tomsword.packet.S2CUseAndromedaPacket;
 public class TomswordClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        ModelLoadingRegistry.INSTANCE.registerModelProvider((resources, out) -> out.accept(new ModelIdentifier("tomsword", "smoldia", "inventory")));
         S2CUseAndromedaPacket.register();
         C2SUseAndromeda.register();
     }
